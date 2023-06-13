41aa35aedf0eba47e515ad0e7d0cfdcb246d3ed4
Pablov <juan.vasquez@valposystem.com>
2023-06-13 19:49:43 -0400

    Resolución de conflictos

--- src/dia_10/Idividual_16.java
+++ src/dia_10/Idividual_16.java
@@ -1,33 +1,34 @@
 package dia_10;
 
 import java.util.Scanner;
+
 public class Idividual_16 {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
         String password = "pass123";
         int contador = 1;
-        System.out.println("ingrese contraseña oportunidad "+contador);
+        System.out.println("ingrese contraseña oportunidad " + contador);
         String cont = leer.nextLine();
         if (cont.equals(password)) {
             System.out.println("la contraseña es correcta");
         } else {
             contador++;
-            System.out.println("Reingrese contraseña. Reingrese contraseña oportunidad "+contador);
+            System.out.println("Reingrese contraseña. Reingrese contraseña oportunidad " + contador);
             String cont2 = leer.nextLine();
             if (cont2.equals(password)) {
                 System.out.println("la contrasenia es correcta");
-            }else{
+            } else {
                 contador++;
-                System.out.println("Contraseña incorrecta. Reingrese la contraseña oportunidad "+contador);
+                System.out.println("Contraseña incorrecta. Reingrese la contraseña oportunidad " + contador);
                 String cont3 = leer.nextLine();
                 if (cont3.equals(password)) {
                     System.out.println("la contrasenia es correcta");
-                }else{
+                } else {
                     contador++;
                     System.out.println("Contraseña incorrecta. No hay mas oportunidades");
                 }
             }
         }
 
-
     }
+}
