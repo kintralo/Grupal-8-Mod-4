cbf347c6b52ced60ee67fd45c707f317f4cc7389
Pablov <juan.vasquez@valposystem.com>
2023-06-13 22:28:34 -0400

    Creación de archivo Word capturas_de_pantalla.docx con contenido

--- type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"dia_9","l":"Grupal_11"},{"p":"<Unnamed>","l":"Grupal_11"},{"p":"dia_8","l":"Grupal8"},{"p":"dia_10","l":"Idividual_16"}];updateSearchResults();
